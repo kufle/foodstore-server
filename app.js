869ccdb33688c09c1882a807702c791427641a7e
kufle <pengetahuanburung@gmail.com>
2021-10-25 21:28:32 +0700

    add category

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 //import product router
 const productRouter = require('./app/product/router');
-const categoryRouter = require('/app/category/router');
+const categoryRouter = require('./app/category/router');
 var app = express();
 
 // view engine setup
@@ -20,6 +20,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 //gunakan product router
 app.use('/api', productRouter);
+app.use('/api', categoryRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
